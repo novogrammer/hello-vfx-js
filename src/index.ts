e79f32a2d9cd54083f5f158c653d4a45e4f425d2
Yusuke Kawamoto <novogrammer@gmail.com>
2026-07-06 19:47:43 +0900

    vwベースのパラメーターを試す

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,32 @@ import { domReady } from './_dom_utils';
 
 
 domReady(()=>{
-  const heroElement = document.querySelector<HTMLElement>('.p-home-section-hero__title-image')!;
 
 
   const vfx = new VFX();
 
-  vfx.add(heroElement, {
-    effect: [new PixelateEffect({ size: 10 }), new BloomEffect({ intensity: 5 })],
+  const heroTitleImageElement = document.querySelector<HTMLElement>('.p-home-section-hero__title-image')!;
+  const pixcelateEffect = new PixelateEffect({ size: 10 });
+  const bloomEffect = new BloomEffect({ intensity: 5 });
+  vfx.add(heroTitleImageElement, {
+    effect: [pixcelateEffect, bloomEffect],
   });
+  setInterval(()=>{
+    pixcelateEffect.setParams({
+      size:Math.round(window.innerWidth / 100),
+    });
+    // bloomEffect.setParams({
+    //   pad:Math.round(window.innerWidth / 100),
+    // });
+  },100);
+
+  // const heroElement = document.querySelector<HTMLElement>('.p-home-section-hero')!;
+  // vfx.add(heroElement, {
+  //   shader: "halftone",
+  // });
+  // setInterval(()=>{
+  //   vfx.update(heroElement);
+  // },100);
 
 })
 
